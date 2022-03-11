5e2959de828b4605bca2f315dd13229eacd0c270
DelinteNicolas <70629561+DelinteNicolas@users.noreply.github.com>
2022-03-11 12:46:37 +0100

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,58 @@
+
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
+# autodoc_mock_imports = []
+
+# -- Project information -----------------------------------------------------
+
+project = 'TIME'
+copyright = '2022, TIME'
+author = 'Nicolas Delinte'
+
+# The full version, including alpha/beta/rc tags
+release = '0.1'
+
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'numpydoc'
+]
+
+numpydoc_show_class_members = False
+autosummary_generate = True
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = [] # = ['_static']
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'

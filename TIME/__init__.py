0f0f45b3f03086051a490edca402527ca2b72aa6
Nicolas Delinte <70629561+DelinteNicolas@users.noreply.github.com>
2022-06-05 23:59:09 +0200

    Update __init__.py
    Updated version number

--- TIME/__init__.py
+++ TIME/__init__.py
@@ -5,5 +5,5 @@ estimates in order to improve the accuracy in the estimation of the
 microstructural properties of neural fibers in a specified tract.
 """
 
-__version__ = "0.0.1a0"
+__version__ = "0.0.2"
 __author__ = 'Nicolas Delinte'

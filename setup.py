14a5d95b68a107d50259ef8ecf80c831f2b9255f
DelinteNicolas <nicolas.delinte@uclouvain.be>
2022-06-09 16:04:42 +0200

    Added pypi description

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     description='Implementation of TIME',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/shuds13/pyexample',
+    url='https://github.com/DelinteNicolas/TIME',
     author='Nicolas Delinte',
     author_email='nicolas.delinte@uclouvain.be',
     license='GNU General Public License v3.0',

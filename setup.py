c5ee9213f112b5455ad44a179d0c3bc9e8f956ab
DelinteNicolas <nicolas.delinte@uclouvain.be>
2022-08-16 00:36:48 +0200

    v0.0.12

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ import TIME
 # read the contents of your README file
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "docs/README.md").read_text()
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='TIME-python',

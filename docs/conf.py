c8dd4d9c849c4174606f87e1953a89abf2d372b8
Nicolas Delinte <nicolas.delinte@uclouvain.be>
2023-08-23 15:16:00 +0200

    Updated theme

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'classic'
 html_static_path = [] # = ['_static']
 
 # -- Options for EPUB output

a5416a65a7f0f6867a4c08c6b0d71dff3c9beadf
Nicolas Delinte <nicolas.delinte@uclouvain.be>
2024-01-18 17:44:21 +0100

    v1.4.18

--- src/unravel/__init__.py
+++ src/unravel/__init__.py
@@ -5,5 +5,5 @@ estimates in order to improve the accuracy in the estimation of the
 microstructural properties of neural fibers in a specified tract.
 """
 
-__version__ = "1.4.17"
+__version__ = "1.4.18"
 __author__ = 'Nicolas Delinte'
